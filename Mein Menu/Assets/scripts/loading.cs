f64446071be84b9f41696ad22b93434c5d48d84c
doshenka <73820956+doshenka@users.noreply.github.com>
2023-03-15 19:23:05 +0400

    15.03.23 изменения

--- Mein Menu/Assets/scripts/loading.cs
+++ Mein Menu/Assets/scripts/loading.cs
@@ -16,13 +16,52 @@ public class loading : MonoBehaviour
 
     void Start()
     {
-        if(SceneManager.GetActiveScene().name == "Scene_loading") StartCoroutine("Async_load_press_key_COR", PlayerPrefs.GetString("current_scene"));
+        if(SceneManager.GetActiveScene().name == "Loading_scene") StartCoroutine("Async_load_press_key_COR", PlayerPrefs.GetString("current_scene")); //current заменить
     }
 
-    // просто с заглушкой
-    public void Simple_load_btn()
+    //----------buttons
+
+    // просто с заглушкой (чисто картинка)
+    /*public void Simple_load_btn()
     {
         container_loading.SetActive(true);
-        //SceneManager.LoadScene;
+        SceneManager.LoadScene(scene_2);
+    }*/
+
+    // асинхронная загрузка (с анимацией загрузки)
+    /*public void Async_load_btn()
+    {
+        StartCoroutine("Async_load_COR");
+    }*/
+
+    // асинхронная загрузка (со сценой)
+    /*public void Scene_load_btn()
+    {
+        PlayerPrefs.SetString("current_scene", scene_2);
+        SceneManager.LoadScene("Loading_scene");
     }
+
+    public void Reload_btn()
+    {
+        container_loading.SetActive(true);
+        SceneManager.LoadScene(0);
+    }*/
+
+    //----загрузка
+
+    //просто асинхр
+    /*IEnumerator Async_load_COR()
+    {
+        float load_progress;
+        async_operation = SceneManager.LoadSceneAsync(scene_2);
+        loading_line.SetActive(true);
+        while(!async_operation.isDone)
+        {
+            load_progress = Mathf.Clamp01(async_operation.progress / 0.9f);
+            loading_text.text = $"loading... {(load_progress * 100).ToString("0")}%"
+        }
+    }*/
+
+
+    //асинхрон для сцены
 }
