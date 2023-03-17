01120ed9e6977aacf1016bd69d67e00fc0715276
doshenka <73820956+doshenka@users.noreply.github.com>
2023-03-17 19:39:32 +0400

    19.03.2023 загрузочный

--- Mein Menu/Assets/scripts/loading.cs
+++ Mein Menu/Assets/scripts/loading.cs
@@ -7,22 +7,41 @@ using UnityEngine.UI;
 public class loading : MonoBehaviour
 {
     public string scene_2 = "level1"; // для перехода к сцене
-    public GameObject loading_line; // для всей загрузки
-    public Image loading_line_image; // спрайт 
-    public Text loading_precent; // процент загрузки
-    public GameObject container_loading; // контейнер отдельный с загрузкой
+    public GameObject progress_bar; // для всей загрузки
+    public Image progress_bar_image; // спрайт 
+    public Text loading_text; // процент загрузки
+    public GameObject loading_image; // контейнер отдельный с загрузкой
 
     AsyncOperation async_operation; // куда сохраняется загрузка другой сцены
 
     void Start()
     {
-        if(SceneManager.GetActiveScene().name == "Scene_loading") StartCoroutine("Async_load_press_key_COR", PlayerPrefs.GetString("current_scene"));
+        if(SceneManager.GetActiveScene().name == "loading") StartCoroutine("Load_press_key_COR", PlayerPrefs.GetString("current_scene"));
     }
 
     // просто с заглушкой
-    public void Simple_load_btn()
+    public void Load_btn()
     {
-        container_loading.SetActive(true);
-        //SceneManager.LoadScene;
+        StartCoroutine("Load_COR");
+    }
+
+    public void Reload_btn()
+    {
+        loading_image.SetActive(true);
+        SceneManager.LoadScene(0);
+    }
+
+    IEnumerator Load_press_key_COR(string scene_name)
+    {
+        float loading_progress;
+        async_operation = SceneManager.LoadSceneAsync(scene_2);
+        progress_bar.SetActive(true);
+        while(!async_operation.isDone)
+        {
+            loading_progress = Mathf.Clamp01(async_operation.progress / 0.9f);
+            loading_text.text = $"Loading ... {(loading_progress * 100).ToString("0")}%";
+            progress_bar_image.fillAmount = loading_progress;
+            yield return null;
+        }
     }
 }
