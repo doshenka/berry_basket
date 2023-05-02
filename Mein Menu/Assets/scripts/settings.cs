0e57095bb33664c91bd80c7f363d2836d5b452be
doshenka <73820956+doshenka@users.noreply.github.com>
2023-05-02 20:25:59 +0400

    other version + script Settings 1/2

---
+++ Mein Menu/Assets/scripts/settings.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class settings : MonoBehaviour
+{
+    public Sprite music_on, music_off;
+
+    void Start () {
+        if (gameObject.name == "Music") {
+            if (PlayerPrefs.GetString () == "off") {
+                GetComponent <Image> ().sprite = music_off;
+                //PlayerPrefs.SetString ("Music", "off"); ПЕРЕПИСАТЬ С КАМЕРОЙ И ОТКЛ МУЗЫКИ
+            } else {
+                GetComponent <Image> ().sprite = music_on;
+                PlayerPrefs.SetString ("Music", "on");
+            }
+        }
+    }
+
+    void SettingsBtn (){
+        switch (gameObject.name) {
+            case "Music":
+                if (PlayerPrefs.GetString ("Music") == "off") {
+                    GetComponent <Image> ().sprite = music_on;
+                    PlayerPrefs.SetString ("Music", "on");
+                } else {
+                    GetComponent <Image> ().sprite = music_off;
+                    PlayerPrefs.SetString ("Music", "off");
+                }
+            /*case "Bright":
+                ;*/
+        }
+    }
+}
